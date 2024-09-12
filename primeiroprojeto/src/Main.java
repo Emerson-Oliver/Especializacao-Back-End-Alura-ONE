9bbf4aeff7812b9965c4442a1238cae5efada46a
Emerson Oliveira <emerson.520@outlook.com>
2024-09-11 21:29:02 -0300

    Add comentarios

--- primeiroprojeto/src/Main.java
+++ primeiroprojeto/src/Main.java
@@ -8,12 +8,14 @@ public class Main {
         System.out.println("Ano de lançamento: " + anoDeLancamento);
         boolean incluidoNoPlano = false;
         float notaDoFilme = 8.1f;
+        //Média calculada por 3 notas 
         float media  = (10.0f + 6.3f + 8.0f) / 3f;
         System.out.printf("%.1f", media);
-        String sinopse = """
-                        Top Gun
-                        Filme de ação e aventura com Ton Cruise!
-                        Ano de lançamento
+        String sinopse;
+        sinopse = """
+                Top Gun
+                Filme de ação e aventura com Ton Cruise!
+                Ano de lançamento
                 """ + anoDeLancamento;
         System.out.println(sinopse);
     }
