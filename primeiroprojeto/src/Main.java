366ba3ca3275afac0e8f5f115fd58fe3e95a72ef
Emerson Oliveira <emerson.520@outlook.com>
2024-09-11 21:45:14 -0300

    Add: casting na media

--- primeiroprojeto/src/Main.java
+++ primeiroprojeto/src/Main.java
@@ -8,9 +8,9 @@ public class Main {
         System.out.println("Ano de lançamento: " + anoDeLancamento);
         boolean incluidoNoPlano = false;
         float notaDoFilme = 8.1f;
-        //Média calculada por 3 notas 
+        //Média calculada por 3 notas
         float media  = (10.0f + 6.3f + 8.0f) / 3f;
-        System.out.printf("%.1f", media);
+        System.out.printf("%.1f%n", media);
         String sinopse;
         sinopse = """
                 Top Gun
@@ -18,5 +18,8 @@ public class Main {
                 Ano de lançamento
                 """ + anoDeLancamento;
         System.out.println(sinopse);
+
+        int classificacao = (int) media / 2;
+        System.out.println(classificacao);
     }
 }
