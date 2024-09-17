6c00281a35ee5d62a99d4760078f8fc337bf5c8c
Emerson Oliveira <emerson.520@outlook.com>
2024-09-16 21:32:08 -0300

    Finalizando o desafio do jogo de adivinhação

--- 2.Condicional/Desafio-1/src/Main.java
+++ 2.Condicional/Desafio-1/src/Main.java
@@ -3,12 +3,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Random random = new Random();
+        int numeroAleatorio = random.nextInt(101);
+
+        int tentativas = 5;
+
         Scanner sc = new Scanner(System.in);
 
-        Random numeroAleatorio = new Random();
+        System.out.println("Bem-vindo ao jogo de adivinhação!");
+        System.out.println("Tente adivinhar o número que estou pensando (entre 0 e 100).");
+        System.out.println("Você tem " + tentativas + " tentativas!");
 
-        System.out.println(numeroAleatorio.nextInt(101));
+        for (int index = 1; index <= tentativas; index+=1) {
+            System.out.print("Tentativa " + index + ": ");
+            int palpite = sc.nextInt();
 
+            if (palpite == numeroAleatorio) {
+                System.out.println("Parabéns! Você acertou o número em " + index + " tentativas.");
+                break;
+            } else if (palpite < numeroAleatorio) {
+                System.out.println("O número é maior do que " + palpite + ".");
+            } else {
+                System.out.println("O número é menor do que " + palpite + ".");
+            }
+            if (index == tentativas) {
+                System.out.println("Você esgotou suas tentativas. O número era " + numeroAleatorio + ".");
+            }
+        }
         sc.close();
     }
 }
